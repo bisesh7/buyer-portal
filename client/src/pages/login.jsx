fa005abe7197bd4af6d5bb2504a0ae51081bd93b
Bisesh Shakya <bisesh.shakya@gmail.com>
2026-04-08 11:06:27 +0545

    Add login page

---
+++ client/src/pages/login.jsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import API from "../services/api";
+import { useNavigate } from "react-router-dom";
+import { Container, Form, Button, Card } from "react-bootstrap";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await API.post("/login", { email, password });
+      localStorage.setItem("token", res.data.token);
+      navigate("/dashboard");
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <Card style={{ width: "400px", padding: "20px" }}>
+        <h2 className="mb-3 text-center">Login</h2>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" onClick={handleLogin} className="w-100">
+            Login
+          </Button>
+        </Form>
+      </Card>
+    </Container>
+  );
+}
