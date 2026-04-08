e197150d837524cca50925cfb6df2ffc40fc11bb
Bisesh Shakya <bisesh.shakya@gmail.com>
2026-04-08 09:34:02 +0545

    add fav controller and add auth middleware

---
+++ server/controllers/favController.js
@@ -0,0 +1,44 @@
+import db from "../db/database.js";
+
+export const getFavoritesByUserId = (req, res) => {
+  const query = `
+        SELECT properties.*
+        FROM favorites
+        JOIN properties ON favorites.property_id = properties.id
+        WHERE favorites.user_id =?
+    `;
+
+  db.all(query, [req.user.id], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json(rows);
+  });
+};
+
+export const addFavorite = (req, res) => {
+  const { property_id } = req.body;
+  const query = "INSERT INTO favorites (user_id, property_id) VALUES (?, ?)";
+
+  db.run(query, [req.user.id, property_id], (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.json({ message: "Property added to favorites" });
+  });
+};
+
+export const removeFavorite = (req, res) => {
+  const { property_id } = req.params;
+  const query = "DELETE FROM favorites WHERE user_id = ? AND property_id = ?";
+
+  db.run(query, [req.user.id, property_id], function (err) {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    if (this.changes === 0) {
+      return res.status(404).json({ error: "Favorite not found" });
+    }
+    res.json({ message: "Property removed from favorites" });
+  });
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,12 @@ import {
   getAllProperties,
   getPropertyById,
 } from "../controllers/propertiesController.js";
+import {
+  getFavoritesByUserId,
+  addFavorite,
+  removeFavorite,
+} from "../controllers/favController.js";
+import auth from "../middlewares/auth.js";
 
 const router = express.Router();
 
@@ -14,5 +20,10 @@ router.post("/login", login);
 //Properties
 router.get("/properties", getAllProperties);
 router.get("/properties/:id", getPropertyById);
+
+//Favorites
+router.get("/favorites", auth, getFavoritesByUserId);
+router.post("/favorites", auth, addFavorite);
+router.delete("/favorites/:property_id", auth, removeFavorite);
 
 export default router;
